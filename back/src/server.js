475c6c8cd439b702c0db1bd32f1edf4b80156598
otokazu.okumura <otokazu.okumura@toyota.global>
2024-05-28 08:50:30 +0900

    微修正

--- back/src/server.js
+++ back/src/server.js
@@ -9,18 +9,19 @@ app.use("/", express.static(__dirname + "/../../front/dist"));
 
 app.get("/api/list/", async (req, res) => {
   const account_list = await knex.select().from("account_book");
-  console.log(new Date().toString(), "---\n", account_list);
+  console.log(new Date().toString(), "\n---account_list---\n", account_list);
   res.send(account_list);
 });
 
-app.post("/api/record/", async (req, res) => {
-  // console.log(req.body);
-  await knex("account_book")
-    .insert(req.body)
-    .then((result) => {});
-  const account_list = await knex.select().from("account_book");
-  console.log(new Date().toString(), "---\n", account_list);
-  res.status(201).end();
+app.post("/api/record/", (req, res) => {
+  console.log(new Date().toString(), "\n---req.body---\n", req.body);
+  res.end();
+  // await knex("account_book")
+  //   .insert(req.body)
+  //   .then((result) => {});
+  // const account_list = await knex.select().from("account_book");
+  // console.log(new Date().toString(), "---\n", account_list);
+  // res.status(201).end();
 });
 
 app.listen(3000, () => {
--- front/src/Form.jsx
+++ front/src/Form.jsx
@@ -1,16 +1,30 @@
 // import React from "react";
 // import { useState, useEffect } from "react";
 
-export default function Form(props) {
+export default function Form() {
+  function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // You can pass formData as a fetch body directly:
+    fetch("/api/record/", { method: form.method, body: formData });
+
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log("formJson.myInput---\n", JSON.parse(formJson.myInput));
+  }
+
   return (
-    <form action="/api/record" method="POST" className="form-post-record">
-      <div className="form-post-record">
-        <label htmlFor="name">Enter your name: </label>
-        <input type="text" name="name" id="name" required />
-      </div>
-      <div className="form-post-record">
-        <input type="submit" value="Subscribe!" />
-      </div>
+    <form method="POST" onSubmit={handleSubmit}>
+      <label>
+        Text input: <input name="myInput" defaultValue="Enter Your Record." />
+      </label>
+      <button type="reset">Reset form</button>
+      <button type="submit">Submit form</button>
     </form>
   );
 }
